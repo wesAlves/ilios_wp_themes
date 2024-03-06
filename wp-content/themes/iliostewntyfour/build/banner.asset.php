a8cc0529f98bbc93dc27dd4af7ff6569d82cbe5b
Wes <wal2.designer@gmail.com>
2024-03-06 17:05:50 +0000

    second round

--- wp-content/themes/iliostewntyfour/build/banner.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '3840b1f95070469403c8');
--- wp-content/themes/iliostewntyfour/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '62b8095c28d0715eaad3');

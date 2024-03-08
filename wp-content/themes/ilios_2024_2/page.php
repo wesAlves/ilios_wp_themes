1766cc634a8d69f113ed0801f7d79eefac0f9156
Wes <wal2.designer@gmail.com>
2024-03-08 10:57:04 +0000

    base structure

--- wp-content/themes/ilios_2024_2/page.php
+++ wp-content/themes/ilios_2024_2/page.php
@@ -1,9 +1,180 @@
 <?php
 get_header();
 ?>
+<main class="h-100">
+    <section class="bg-brand-gray-1 py-5">
+        <div class="container">
+            <h2>Sobre o escritório</h2>
 
-page.php
+            <div class="row mt-5">
+                <div class="col-6">
+                    <p>Situado no centro de Florianópolis/SC, o escritório visa à prestação de serviços jurídicos com
+                        qualidade, honestidade, segurança e eficiência no âmbito consultivo, consensual, contencioso e
+                        preventivo para pessoas físicas ou jurídicas.</p>
 
+                    <p>A boa base profissional será o alicerce para o relacionamento advogado/cliente, sendo
+                        imprescindível
+                        a orientação precisa ao cliente acerca do direito pretendido ou defendido e tudo mais que disser
+                        respeito ao processo já em curso ou ao eventual futuro processo em todas as suas fases.</p>
+
+                    <p>Nossos parceiros acompanham a evolução do direito em decorrência das constantes mudanças na
+                        legislação nacional e a modernidade da justiça, já que a nova realidade do mercado jurídico
+                        impõe a
+                        especialização e constante atualização do conhecimento nas ações profissionais.</p>
+                </div>
+
+                <div class="col-6">
+                    <p>Tudo para que os trabalhos sejam feitos com a agilidade e a segurança de se cumprir prazos
+                        somados à
+                        excelência dos serviços que o cliente merece. </p>
+
+                    <p>Compromisso com causas sociais e com a defesa dos direitos humanos. A advocacia tem o dever de
+                        construir uma sociedade melhor.</p>
+
+                    <p>Os clientes têm a certeza de que seus processos estão sob constante vigilância profissional. A
+                        tríade
+                        conhecimento, experiência e reputação profissional é a marca que distingue o escritório e
+                        proporciona a seus clientes soluções e resultados efetivos para a tomada de decisões e redução
+                        de
+                        riscos.</p>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+    <section class="py-5">
+        <div class="container">
+            <h2>Áreas de atuação</h2>
+
+            <div class="row mt-5">
+                <div class="p-0 m-0 d-flex flex-wrap list-unstyled">
+                    <div class="w-25">
+                        <img src="" alt="">
+                        <h3>Direito trabalhista para empresas</h3>
+                    </div>
+                    <div class="w-25">
+                        <img src="" alt="">
+                        <h3>Direito odontológico para dentistas</h3>
+                    </div>
+                    <div class="w-25">
+                        <img src="" alt="">
+                        <h3>Direito Esteticista para profissionais</h3>
+                    </div>
+                    <div class="w-25">
+                        <img src="" alt="">
+                        <h3>Direito laboratorial</h3>
+                    </div>
+                    <div class="w-25">
+                        <img src="" alt="">
+                        <h3>Direito médico para médicos</h3>
+                    </div>
+                    <div class="w-25">
+                        <img src="" alt="">
+                        <h3>Atuação em tribunais direito geral</h3>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="bg-brand-gray-1">
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <img src="wp-content/themes/ilios_2024_2/build/assets/fotos_rossana/rossana_fundo_claro_11_23 2.png"
+                         alt="" class="w-100">
+                </div>
+
+                <div class="col-8 p-5">
+                    <h2>Quem é Dr. Rossana Mansur Colier</h2>
+
+                    <p>Bacharel em direito pela Pontifícia Universidade Católica de Minas Gerais(PUC-MG) em 1993.</p>
+
+                    <p>Possui pós-graduações em Direito Processual Civil, Direito Processual do Trabalho e Direito
+                        Público Municipal. Possui ainda especialização em Direito Médico.</p>
+
+                    <p>Como proprietária de escritório de advocacia nos Estados de Minas Gerais e Santa Catarina atuou
+                        como advogada de forma efetiva e ininterrupta durante 30 anos.</p>
+
+                    <p>Dentre diversas atividades jurídicas prestou serviços como advogada para empresas, autarquia
+                        federal, procuradoria jurídica municipal, bem como atuações perante Conselhos Regionais de
+                        Medicina.</p>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="py-5 text-white">
+        <div class="container">
+            <h2>Contatos e localização</h2>
+
+            <div class="row">
+                <div class="col-4">
+                    <p>Rua Feliciano Nunes Pires, 168, Ed. Igor , Salas 05, Centro, Florianópolis - 88015-220.</p>
+
+                    <p>Celular / WhatsApp: (48) 99961 2711</p>
+
+                    <p>Fone: (48) 3207 - 0938</p>
+
+                    <p>E-mail: rossanamc.adv@hotmail.com</p>
+
+                    <p>De Segunda a Sexta-feira das 10:00 as 17:00</p>
+                </div>
+                <div class="col-6 offset-2">
+                    <form action="">
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1"
+                                   aria-describedby="emailHelp">
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <div class="mb-3 form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-5">
+
+            <iframe width="100%" height="272"
+                    src="https://www.openstreetmap.org/export/embed.html?bbox=-48.549227714538574%2C-27.592694778307255%2C-48.54485839605332%2C-27.590450733624795&amp;layer=mapnik&amp;marker=-27.59157276170745%2C-48.547043055295944"
+                    style="border: 1px solid transparent">
+            </iframe>
+
+            <br/>
+
+            <small>
+                <a class="px-3" href="https://www.openstreetmap.org/#map=19/-27.59178/-48.54734&amp;layers=N" target="_blank">View Larger Map</a>
+            </small>
+        </div>
+
+    </section>
+
+    <section class="py-5 text-white">
+        <div class="container">
+            <h2>Confira nosso blog</h2>
+
+            <div class="d-flex mt-5 gap-3">
+                <div class="col-4 bg-brand-gray-1">card 1</div>
+                <div class="col-4 bg-brand-gray-1">card 2</div>
+                <div class="col-4 bg-brand-gray-1">card 3</div>
+            </div>
+
+        </div>
+    </section>
+</main>
 <?php
 get_footer();
 ?>

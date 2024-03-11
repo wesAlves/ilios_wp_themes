14e4d9c4d9adf57b10981d6196744f008bae4fcd
Wes <wal2.designer@gmail.com>
2024-03-11 12:46:08 +0000

    fixing menu

--- src/js/slider/index.js
+++ src/js/slider/index.js
@@ -4,7 +4,12 @@ import Splide from '@splidejs/splide';
 
 const homePath = window.location
 
-if (`${homePath.origin}/` === homePath.href) {
+const current = homePath.href.split("#");
+const origin = `${homePath.origin}/`;
+
+console.log(origin, current);
+
+if (origin === current[0]) {
     new Splide('.splide', {
         type: 'loop',
         autoplay: 'true',

4fd3735aa23b33b7c5e7d9414e43d10d65b21850
Wes <wal2.designer@gmail.com>
2024-03-12 10:03:32 +0000

    updating blog page

--- wp-content/themes/ilios_2024_2/index.php
+++ wp-content/themes/ilios_2024_2/index.php
@@ -1,10 +1,15 @@
 <?php
 get_header();
 ?>
-<div style="height: 340px" class="bg-brand-yellow-3 d-flex align-items-end">
+<div style="height: 340px; background-image: url(<?php echo get_theme_file_uri('/build/assets/headers/justice.png') ?>), linear-gradient(#cb9f6000, black);"
+     class="banner-page bg-brand-yellow-3 d-flex align-items-end">
     <div class="container pb-5">
-        <h1>Blog</h1>
-        <p>Blog description</p>
+        <div class="col-5">
+        <h1 style="color: #333333">Blog</h1>
+        <p style="font-size: 24px; color: #333333" class="mb-0">Seja bem vindo ao blog do site Rossana Advocacia.</p>
+        <p style="font-size: 24px;  color: #333333">Aqui você encontrará notícias e matérias relavantes às áreas do direito.</p>
+
+        </div>
     </div>
 </div>
 

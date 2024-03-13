be127daf07eb2e55b8c4afc09587ca52ca06d144
Wes <wal2.designer@gmail.com>
2024-03-13 12:18:14 +0000

    webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.s[ac]ss$/i,
+                test: /\.s?[ac]ss$/i,
                 use: [
                     "style-loader",
                     "css-loader",
@@ -24,11 +24,8 @@ module.exports = {
                         },
                     },
                 ],
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
             }
         ],
     },
+
 }

058a975e1d8b3942aba6a0cf36ae54afe1f8bb15
Wes <wal2.designer@gmail.com>
2024-08-09 10:55:41 +0100

    wip: updating all sections

--- wp-content/themes/ilios_2024_2/front-page.php
+++ wp-content/themes/ilios_2024_2/front-page.php
@@ -2,6 +2,7 @@
 get_header();
 ?>
 
+<!--TODO: isolate that section in a component and embed here-->
 <section class="splide" aria-label="carroulsel">
     <div class="splide__track">
         <ul class="splide__list">
@@ -9,14 +10,15 @@ get_header();
                 style="background: url(<?php echo get_theme_file_uri("/build/assets/hero-images/slide_1.png") ?>); background-size: cover; background-position: center center">
                 <div class="container h-100 d-flex align-items-center">
                     <div class="col-md-6 px-5 px-md-auto">
-                        <h5 class="mb-4 slider-title with-bar">
+                        <h5 class="mb-4 slide-title with-bar">
                             Texto de impacto 01
                         </h5>
 
-                        <p style="color: #fff">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat augue sed velit
+                        <p style="color: #fff">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat
+                            augue sed velit
                             accumsan gravida eget vitae libero.</p>
 
-                        <a href="#contact" class="btn btn-tertiary" style="color: #fff">Entre com contato</a>
+                        <a href="#contact" class="btn btn-tertiary " style="color: #fff">Entre com contato</a>
                     </div>
                 </div>
             </li>
@@ -25,11 +27,12 @@ get_header();
                 style="background: url(<?php echo get_theme_file_uri("/build/assets/hero-images/slide_2.png") ?>); background-size: cover; background-position: center center">
                 <div class="container h-100 d-flex align-items-center">
                     <div class="col-md-6 offset-md-6 px-5 px-md-auto">
-                        <h5 class="mb-4 slider-title with-bar">
+                        <h5 class="mb-4 slide-title with-bar">
                             Texto de impacto 02
                         </h5>
 
-                        <p style="color: #fff">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat augue sed velit
+                        <p style="color: #fff">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat
+                            augue sed velit
                             accumsan gravida eget vitae libero.</p>
 
                         <a href="#contact" class="btn btn-tertiary" style="color: #fff">Entre com contato</a>
@@ -43,171 +46,133 @@ get_header();
 </header>
 
 <main class="h-100">
-    <!--    <section class="bg-brand-gray-1 py-5" id="about">-->
-    <!--        <div class="container">-->
-    <!--            <h2 class="text-center mx-auto">Sobre a Trhabe</h2>-->
-    <!---->
-    <!--            <div class="div d-md-flex mt-5">-->
-    <!---->
-    <!--                aqui vem os cards para os serviços e produtos-->
-    <!---->
-    <!--            </div>-->
-    <!---->
-    <!--        </div>-->
-    <!--    </section>-->
-
-    <section class="py-5 text-white">
+    <section class="bg-brand-gray-1 py-5" id="featured-products">
         <div class="container">
-            <h2 class="text-left">Produtos em destaque</h2>
+            <h2 class="text-left with-bar">Produtos em destaque</h2>
 
-            <div class="my-5">
-                <div class="p-0 m-0 d-md-flex flex-wrap text-decoration-none">
-                    <!--                    <a href="-->
-                    <?php //echo get_permalink(56) ?><!--" class="text-decoration-none col-md-4 d-flex flex-column align-items-center mt-5">-->
-                    <!--                        <div style="width: 204px; height: 204px; overflow: hidden; display: flex; justify-content: center; align-items: center">-->
-                    <!--                            <img src="-->
-                    <?php //echo get_theme_file_uri("/build/assets/areas/direto_geral.png") ?><!--" alt="">-->
-                    <!--                        </div>-->
-                    <!--                        <h3 class="mt-3 text-center w-75">Direito Trabalhista <br/><span-->
-                    <!--                                    class="small text-white-50">para empresas</span>-->
-                    <!--                        </h3>-->
-                    <!--                    </a>-->
-                    <!---->
-                    <!--                    <a href="-->
-                    <?php //echo get_permalink(56) ?><!--" class="text-decoration-none col-md-4 d-flex flex-column align-items-center mt-5">-->
-                    <!--                        <div style="width: 204px; height: 204px; overflow: hidden; display: flex; justify-content: center; align-items: center">-->
-                    <!--                            <img src="-->
-                    <?php //echo get_theme_file_uri("/build/assets/areas/direto_geral.png") ?><!--" alt="">-->
-                    <!--                        </div>-->
-                    <!--                        <h3 class="mt-3 text-center w-75">Direito Odontológico <br/><span-->
-                    <!--                                    class="small text-white-50">para dentistas</span>-->
-                    <!--                        </h3>-->
-                    <!--                    </a>-->
-                    <!--                    <a href="-->
-                    <?php //echo get_permalink(56) ?><!--" class="text-decoration-none col-md-4 d-flex flex-column align-items-center mt-5">-->
-                    <!--                        <div style="width: 204px; height: 204px; overflow: hidden; display: flex; justify-content: center; align-items: center">-->
-                    <!--                            <img src="-->
-                    <?php //echo get_theme_file_uri("/build/assets/areas/direto_geral.png") ?><!--" alt="">-->
-                    <!--                        </div>-->
-                    <!--                        <h3 class="mt-3 text-center w-75">Direito Esteticista <br/><span-->
-                    <!--                                    class="small text-white-50">para profissionais</span>-->
-                    <!--                        </h3>-->
-                    <!--                    </a>-->
-                    <!--                    <a href="-->
-                    <?php //echo get_permalink(56) ?><!--" class="text-decoration-none col-md-4 d-flex flex-column align-items-center mt-5">-->
-                    <!--                        <div style="width: 204px; height: 204px; overflow: hidden; display: flex; justify-content: center; align-items: center">-->
-                    <!--                            <img src="-->
-                    <?php //echo get_theme_file_uri("/build/assets/areas/direto_geral.png") ?><!--" alt="">-->
-                    <!--                        </div>-->
-                    <!--                        <h3 class="mt-3 text-center w-75">Direito laboratorial</h3>-->
-                    <!--                    </a>-->
-                    <!--                    <a href="-->
-                    <?php //echo get_permalink(56) ?><!--" class="text-decoration-none col-md-4 d-flex flex-column align-items-center mt-5">-->
-                    <!--                        <div style="width: 204px; height: 204px; overflow: hidden; display: flex; justify-content: center; align-items: center">-->
-                    <!--                            <img src="-->
-                    <?php //echo get_theme_file_uri("/build/assets/areas/direto_geral.png") ?><!--" alt="">-->
-                    <!--                        </div>-->
-                    <!--                        <h3 class="mt-3 text-center w-75">Direito Médico<br/>-->
-                    <!--                            <span class="small text-white-50">para médicos</span>-->
-                    <!--                        </h3>-->
-                    <!--                    </a>-->
-                    <!--                    <a href="-->
-                    <?php //echo get_permalink(56) ?><!--" class="text-decoration-none col-md-4 d-flex flex-column align-items-center mt-5">-->
-                    <!--                        <div style="width: 204px; height: 204px; overflow: hidden; display: flex; justify-content: center; align-items: center">-->
-                    <!--                            <img src="-->
-                    <?php //echo get_theme_file_uri("/build/assets/areas/direto_geral.png") ?><!--" alt="">-->
-                    <!--                        </div>-->
-                    <!--                        <h3 class="mt-3 text-center w-75">Atuação em tribunais<br/><span-->
-                    <!--                                    class="small text-white-50">Direito geral</span>-->
-                    <!--                        </h3>-->
-                    <!--                    </a>-->
+            <div class="div d-md-flex mt-5">
 
+                <!--                TODO: make a loop here and a custom post type-->
+                <div class="card" style="max-width: 345px">
+                    <img src="<?php //echo get_theme_file_uri("/build/assets/areas/direto_geral.png") ?>" alt=""
+                         class="card-img-top">
+                    <div class="card-body">
+                        <h3 class="card-title fw-bold">Nome do item ofertado em destaque</h3>
+                        <p class="card-text">
+                            Descrição do produto e com algum conteúdo relevante ao que o produto pode ser relacionado ou
+                            para o que serve.
+                        </p>
+                    </div>
                 </div>
+
             </div>
 
         </div>
     </section>
 
-    <section class="bg-brand-gray-1">
+    <section class="py-5 text-white" id="about">
+        <div class="container">
+            <h2 class="text-left with-bar">Quem é Thrabe e qual nossa missão</h2>
+
+            <div class="p-0 m-0 d-md-flex flex-wrap text-decoration-none">
+                <p class="col-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat augue sed
+                    velit accumsan
+                    gravida eget vitae libero. Cras dignissim convallis orci vitae imperdiet. In ut purus sit amet
+                    lacus consequat malesuada vitae quis mi. Phasellus ornare eu leo at bibendum. In pharetra
+                    iaculis sodales. Vivamus id eros bibendum, luctus odio non, fringilla odio. </p>
+
+            </div>
+
+            <a href="#contact" class="btn btn-tertiary " style="color: #fff">Entre com contato</a>
+
+        </div>
+    </section>
+
+    <section class="bg-brand-gray-1 py-5" id="services">
         <div class="container">
             <div class="row">
-                <div class="col-md-4 p-0 p-md-auto">
-                    <img src="wp-content/themes/ilios_2024_2/build/assets/fotos_rossana/rossana_fundo_claro_11_23 2.png"
-                         alt="" class="w-100">
-                </div>
+                <h2 class="text-left with-bar">Serviços</h2>
 
-                <div class="col-md-8 py-5 px-md-5">
-                    <h2 class="text-center mx-md-auto p-0">Quem é Dr. Rossana Mansur Colier</h2>
+                <p class="my-0">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat augue sed velit accumsan
+                    gravida eget vitae libero. Cras dignissim convallis orci vitae imperdiet. In ut purus sit amet lacus
+                    consequat malesuada vitae quis mi. Phasellus ornare eu leo at bibendum. In pharetra iaculis sodales.
+                    Vivamus id eros bibendum, luctus odio non, fringilla odio. 
+                </p>
 
-                    <p>Bacharel em direito pela Pontifícia Universidade Católica de Minas Gerais(PUC-MG) em 1993.</p>
+                <div class="div d-md-flex mt-5">
 
-                    <p>Possui pós-graduações em Direito Processual Civil, Direito Processual do Trabalho e Direito
-                        Público Municipal. Possui ainda especialização em Direito Médico.</p>
-
-                    <p>Como proprietária de escritório de advocacia nos Estados de Minas Gerais e Santa Catarina atuou
-                        como advogada de forma efetiva e ininterrupta durante 30 anos.</p>
-
-                    <p>Dentre diversas atividades jurídicas prestou serviços como advogada para empresas, autarquia
-                        federal, procuradoria jurídica municipal, bem como atuações perante Conselhos Regionais de
-                        Medicina.</p>
+                    <!--                TODO: make a loop here and a custom post type-->
+                    <div class="card" style="max-width: 345px">
+                        <img src="<?php //echo get_theme_file_uri("/build/assets/areas/direto_geral.png") ?>" alt=""
+                             class="card-img-top">
+                        <div class="card-body">
+                            <h3 class="card-title fw-bold">Nome do item ofertado em destaque</h3>
+                            <p class="card-text">
+                                Descrição do produto e com algum conteúdo relevante ao que o produto pode ser
+                                relacionado ou
+                                para o que serve.
+                            </p>
+                        </div>
+                    </div>
 
                 </div>
             </div>
         </div>
     </section>
 
-
-    <?php
-
-    $posts = get_posts(array(
-        "numberposts" => 3
-    ));
-    $counter = 0;
-
-    if (count($posts)) {
-
-        ?>
-
-        <section class="py-5 text-white">
-            <div class="container">
-                <h2 class="text-center mx-auto">Confira nosso blog</h2>
-
-                <div class="d-md-flex mt-5 gap-3">
-
-
-                    <?php
-                    while ($counter < count($posts)) {
-                        the_post();
-                        ?>
-
-                        <a href="<?php the_permalink(); ?>" class="col-md-4 card mt-5 mt-md-0 ">
-
-                            <div class=" w-100" alt="...">
-                                <img src="<?php the_post_thumbnail_url("large"); ?>"
-                                     alt="feature" class="w-100">
-                            </div>
-
-                            <div class="card-body">
-                                <h5 class="card-title"><?php the_title(); ?></h5>
-                                <p class="card-text">
-                                    <?php the_excerpt(); ?>
-                                </p>
-
-                            </div>
-                            <!--                            </div>-->
-                        </a>
-
-                        <?php
-                        $counter++;
-                    }
-                    ?>
-
-                </div>
-
-            </div>
-        </section>
-
-    <?php } ?>
+    <!--BLOG WHEN AVALIABLE UNCOMMENT THAT-->
+    <!--    --><?php
+    //
+    //    $posts = get_posts(array(
+    //        "numberposts" => 3
+    //    ));
+    //    $counter = 0;
+    //
+    //    if (count($posts)) {
+    //
+    //        ?>
+    <!---->
+    <!--        <section class="py-5 text-white">-->
+    <!--            <div class="container">-->
+    <!--                <h2 class="text-center mx-auto">Confira nosso blog</h2>-->
+    <!---->
+    <!--                <div class="d-md-flex mt-5 gap-3">-->
+    <!---->
+    <!---->
+    <!--                    --><?php
+    //                    while ($counter < count($posts)) {
+    //                        the_post();
+    //                        ?>
+    <!---->
+    <!--                        <a href="--><?php //the_permalink(); ?><!--" class="col-md-4 card mt-5 mt-md-0 ">-->
+    <!---->
+    <!--                            <div class=" w-100" alt="...">-->
+    <!--                                <img src="--><?php //the_post_thumbnail_url("large"); ?><!--"-->
+    <!--                                     alt="feature" class="w-100">-->
+    <!--                            </div>-->
+    <!---->
+    <!--                            <div class="card-body">-->
+    <!--                                <h5 class="card-title">--><?php //the_title(); ?><!--</h5>-->
+    <!--                                <p class="card-text">-->
+    <!--                                    --><?php //the_excerpt(); ?>
+    <!--                                </p>-->
+    <!---->
+    <!--                            </div>-->
+    <!--                            <!--                            </div>-->-->
+    <!--                        </a>-->
+    <!---->
+    <!--                        --><?php
+    //                        $counter++;
+    //                    }
+    //                    ?>
+    <!---->
+    <!--                </div>-->
+    <!---->
+    <!--            </div>-->
+    <!--        </section>-->
+    <!---->
+    <!--    --><?php //} ?>
 
 </main>
 
